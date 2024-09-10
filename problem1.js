85552f7494a9a8bd6c35774214e32815a0e2c654
Hasan1971f <mdhasanurrahmanhasan101@gmail.com>
2024-09-10 08:41:39 +0600

    added a function problem1.js

--- problem1.js
+++ problem1.js
@@ -1,17 +1,35 @@
 
-const numbers = [2,4,21,5,6,23,20,9]
+// const numbers = [2,4,21,5,6,23,20,9]
 
 
 // const max =Math.max(2,4,21,5,6,23,20,9)
 // console.log(max)
 
-let biggest = numbers[0]
-console.log('before loop:', biggest)
-for (let i = 1; i < numbers.length; i++ ) {
-    console.log("current number:", numbers[i], "biggest number:", biggest)
-    if(numbers[i] > biggest){
-        biggest = numbers[i]
+// let biggest = numbers[0]
+// console.log('before loop:', biggest)
+// for (let i = 1; i < numbers.length; i++ ) {
+//     console.log("current number:", numbers[i], "biggest number:", biggest)
+//     if(numbers[i] > biggest){
+//         biggest = numbers[i]
+//     }
+//     console.log("current biggest:", biggest)
+// }
+// console.log('after loop:', biggest)
+
+
+
+// _________________Function use_____________________
+
+function giveBiggestNumber(array) {
+    let biggest = array[0]
+    for (let i = 1; i< array.length; i++) {
+        if(array[i] > biggest){
+            biggest = array[i]
+        }
+        
     }
-    console.log("current biggest:", biggest)
+    return biggest
 }
-console.log('after loop:', biggest)
+const numbers = [2,4,21,5,6,23,20,9]
+const result = giveBiggestNumber(numbers)
+console.log(result)
