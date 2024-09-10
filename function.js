5d291ec491a8d6a94ab72e75e64d19c89c635212
Hasan1971f <mdhasanurrahmanhasan101@gmail.com>
2024-09-10 08:06:52 +0600

    added a function commit

---
+++ function.js
@@ -0,0 +1,49 @@
+// function shoeName(params) {
+//     console.log("Hasan")
+// }
+// shoeName()
+
+// function shoeName(name) {
+//         console.log(name)
+//     }
+//     shoeName("hasan")
+
+// ______________________Jog kora____________________
+
+//  function add(x, y) {
+//     const total = x + y
+//     console.log(total)
+//  }
+//  add(4, 7)
+//  add(5, 4)
+
+// function giveName() {
+//     const x = "Hasan"
+//     return x
+// }
+// const myName = giveName()
+// console.log(myName)
+
+// ___________________typeof Function____________________
+
+// Types of function(4) 
+//  1. No parameters No return
+function show() {
+    console.log("hello")
+}
+//  2. Parameters with no return
+function add(x, y) {
+    console.log(x + y)
+}
+//  3. No parameters But return
+
+function giveName() {
+        const x = "Hasan"
+        return x
+    }
+
+// 4. Parameter with return
+
+function giveDouble(x) {
+    return x*2
+}
---
+++ problem1.js
@@ -0,0 +1,17 @@
+
+const numbers = [2,4,21,5,6,23,20,9]
+
+
+// const max =Math.max(2,4,21,5,6,23,20,9)
+// console.log(max)
+
+let biggest = numbers[0]
+console.log('before loop:', biggest)
+for (let i = 1; i < numbers.length; i++ ) {
+    console.log("current number:", numbers[i], "biggest number:", biggest)
+    if(numbers[i] > biggest){
+        biggest = numbers[i]
+    }
+    console.log("current biggest:", biggest)
+}
+console.log('after loop:', biggest)
